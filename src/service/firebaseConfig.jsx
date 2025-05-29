1bc9cc3932f05be9398884e4e9333edf13d4bff8
jeevesh0013 <138964944+jeevesh0013@users.noreply.github.com>
2025-05-29 19:39:51 +0530

    Update firebaseConfig.jsx

--- src/service/firebaseConfig.jsx
+++ src/service/firebaseConfig.jsx
@@ -8,12 +8,12 @@ import { getFirestore} from "firebase/firestore"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCdTctA3l111ekwOKNoCohmtEakOAoIYOU",
+  apiKey: "*************",
   authDomain: "ai-trip-planner-ec051.firebaseapp.com",
   projectId: "ai-trip-planner-ec051",
   storageBucket: "ai-trip-planner-ec051.appspot.com",
   messagingSenderId: "436472852902",
-  appId: "1:436472852902:web:379070bd0ff6df0f08df67",
+  appId: "**************",
   measurementId: "G-BX3BXRBPCS"
 };
 
